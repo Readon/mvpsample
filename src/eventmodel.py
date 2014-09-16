19bf9d16808ba79994846cc504beb4128a9e3d65
Yindong <xydarcher@uestc.edu.cn>
2014-09-16 15:07:49 +0800

    new model prototype for two different library.

--- src/eventmodel.py
+++ src/eventmodel.py
@@ -1,9 +1,47 @@
 
-from eventize import Attribute
-from mvp import Model
+from eventize import handle, Attribute
+from mvp import Model as Base
 
 
-class EventModel(Model):
+class Model(Base):
     def __init__(self):
-        super(EventModel, self).__init__()
+        super(Model, self).__init__()
+        self._connections = {}
         return
+
+    def is_bindable(self, entry):
+        item = getattr(self, entry)
+        return not callable(item)
+
+    def connect(self, entry, action):
+        if entry not in self._connections:
+            connection = handle(self, entry)
+            self._connections[entry] = connection
+        else:
+            connection = self._connections[entry]
+        connection.on_change += action
+        return self._convertion
+
+    def disconnect(self, entry, action):
+        if entry in self._connections:
+            connection = self._connections[entry]
+            connection.on_change -= action
+        return
+
+    def _convertion(self, event):
+        return event.value
+
+if __name__ == '__main__':
+    class MyModel(Model):
+        text = ""
+
+    model = MyModel()
+    func = None
+
+    def foo(event):
+        print func(event)
+
+    if model.is_bindable("text"):
+        func = model.connect("text", foo)
+    setattr(model, "text", "123")
+    print model.text
--- src/traitsmodel.py
+++ src/traitsmodel.py
@@ -1,9 +1,40 @@
 
-from traits.api import HasTraits
-from mvp import Model
+from traits.api import HasTraits, TraitType
+from mvp import Model as Base
 
 
-class TraitsModel(HasTraits, Model):
+class Model(HasTraits, Base):
     def __init__(self):
-        super(TraitsModel, self).__init__()
+        super(Model, self).__init__()
         return
+
+    def is_bindable(self, entry):
+        return entry in self.class_trait_names()
+
+    def connect(self, entry, action):
+        self.on_trait_change(action, entry)
+        return self._convertion
+
+    def disconnect(self, entry, action):
+        self.on_trait_change(action, entry, remove=True)
+        return
+
+    def _convertion(self, event):
+        return event
+
+
+from traits.api import String
+if __name__ == '__main__':
+    class MyModel(Model):
+        text = String("")
+
+    model = MyModel()
+    func = None
+
+    def foo(event):
+        print func(event)
+
+    if model.is_bindable("text"):
+        func = model.connect("text", foo)
+    setattr(model, "text", "123")
+    print model.text
