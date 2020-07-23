03dbed1454347a422b944457addf5002ea4db742
Yindong <xydarcher@uestc.edu.cn>
2020-07-24 00:12:22 +0800

    try to fit with latest tools.

--- cython_setup.py
+++ cython_setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from pathlib2 import Path
 
-try:    
+try:
     from setuptools.command.build_py import build_py as _build_py
     from setuptools import Extension
 except ImportError:
@@ -18,17 +18,16 @@ except ImportError:
 
 from Cython.Build import cythonize
 
-from distutils.msvccompiler import MSVCCompiler
-msvc = MSVCCompiler()
-msvc.initialize()
-del msvc
+# from distutils.msvccompiler import MSVCCompiler
+# msvc = MSVCCompiler()
+# msvc.initialize()
+# del msvc
 
-INIT_HIDEN_NAME = u'_init'
-INIT_HIDEN_FILE = INIT_HIDEN_NAME + '.py'
+INIT_HIDEN_NAME = u"_init"
+INIT_HIDEN_FILE = INIT_HIDEN_NAME + ".py"
 
 
 class Cython(_build_py):
-
     @staticmethod
     def _delete_path(path):
         if Path(path).exists():
@@ -36,35 +35,34 @@ class Cython(_build_py):
                 shutil.rmtree(str(path))
             else:
                 os.unlink(str(path))
-    
+
     def cleanup_templatefiles(self):
         try:
             len(self._cython_templatefiles)
         except AttributeError:
             return
-        
+
         for file_ in self._cython_templatefiles:
             Cython._delete_path(file_)
-            Cython._delete_path(file_.with_suffix('.pyc'))
-            Cython._delete_path(file_.with_suffix('.c'))
+            Cython._delete_path(file_.with_suffix(".pyc"))
+            Cython._delete_path(file_.with_suffix(".c"))
 
     def build_module(self, module, module_file, package):
-        from nuitka.importing.Importing import setMainScriptDirectory
         dist = self.distribution
-        if not dist.ext_modules or '' in dist.ext_modules:
+        if not dist.ext_modules or "" in dist.ext_modules:
             dist.ext_modules = []
 
-        package_blocks = package.split('.')
+        package_blocks = package.split(".")
 
         src_path = Path(module_file)
-        infile = self.get_module_outfile(self.build_lib, package_blocks, module) 
+        infile = self.get_module_outfile(self.build_lib, package_blocks, module)
         original = Path(infile)
 
         if module == "__init__":
             final_pyd = original.parent / INIT_HIDEN_FILE
-            final_pyd = final_pyd.with_suffix('.pyd')
+            final_pyd = final_pyd.with_suffix(".pyd")
         else:
-            final_pyd = original.with_suffix('.pyd')
+            final_pyd = original.with_suffix(".pyd")
 
         if final_pyd.exists():
             dst_mtime = final_pyd.stat().st_mtime
@@ -72,13 +70,13 @@ class Cython(_build_py):
             if src_mtime < dst_mtime:
                 return
         _build_py.build_module(self, module, module_file, package)
-                
+
         if module == "__init__":
             new_name = original.with_name(INIT_HIDEN_FILE)
             if new_name.exists():
                 self._delete_path(new_name)
             original.rename(new_name)
-            with original.open('w') as stub_init:
+            with original.open("w") as stub_init:
                 stub_init.write("from .%s import *" % INIT_HIDEN_NAME)
             original = new_name
             module = INIT_HIDEN_NAME
@@ -87,14 +85,16 @@ class Cython(_build_py):
         cwd = original.parent
         self._delete_path(final_pyd)
 
-        #print("compiling %s -> %s" % (str(original), final_pyd))
+        # print("compiling %s -> %s" % (str(original), final_pyd))
         try:
             len(self._cython_templatefiles)
         except AttributeError:
             self._cython_templatefiles = []
-        
+
         self._cython_templatefiles.append(original)
-        module_name = str('.'.join([package, module]))
+        module_name = module
+        if package:
+            module_name = str(".".join([package, module]))
         extension = Extension(module_name, [str(original)])
-        dist.ext_modules += cythonize([extension])
+        dist.ext_modules += cythonize([extension], language_level=2)
 
--- setup.py
+++ setup.py
@@ -6,46 +6,41 @@ except ImportError:
     from distutils.core import setup, find_packages
     from distutils.extension import Extension
 
-#from nuitka_setup import Nuitka
+# from nuitka_setup import Nuitka
 from pyinstaller_setup import run as pyinstaller_run
 from cython_setup import Cython
 
-APP_NAME = 'MVPSample'
-PACKAGE_DIR = 'src'
-PACKAGES = [''] + find_packages(PACKAGE_DIR)
-STARTUP_SCRIPT = PACKAGE_DIR + r'\startup.pyw'
-EXCLUDE_PACKAGES = ['PySide', 'PyQt', '_tkinter']
+APP_NAME = "MVPSample"
+PACKAGE_DIR = "src"
+PACKAGES = [""] + find_packages(PACKAGE_DIR)
+STARTUP_SCRIPT = PACKAGE_DIR + r"\startup.pyw"
+EXCLUDE_PACKAGES = ["PySide", "PyQt", "_tkinter"]
 
-DATA_FILES = [
-              '*.ui',
-              '*.glade'
-              ]
+DATA_FILES = ["*.ui", "*.glade"]
 
 RUNTIME_DEPS = []
-BUILD_DEPS = ['pyinstaller >= 3.3', 'cython', 'pathlib']
+BUILD_DEPS = ["pyinstaller >= 3.3", "cython", "pathlib"]
 
 if __name__ == "__main__":
     ret = setup(
-        name = APP_NAME,
-        version = '0.1',
-        description = 'Awg waveform generation and edit application.',
-        author = 'Yindong Xiao',
-        author_email = 'xydarcher@uestc.edu.cn',
-
-        install_requires = RUNTIME_DEPS,
-        setup_requires = BUILD_DEPS,
-
-        packages = PACKAGES,
-        package_dir = {'': PACKAGE_DIR},
-        package_data = {'': DATA_FILES},
-        scripts = [STARTUP_SCRIPT],
-        cmdclass={'build_py': Cython}
+        name=APP_NAME,
+        version="0.1",
+        description="Awg waveform generation and edit application.",
+        author="Yindong Xiao",
+        author_email="xydarcher@uestc.edu.cn",
+        install_requires=RUNTIME_DEPS,
+        setup_requires=BUILD_DEPS,
+        packages=PACKAGES,
+        package_dir={"": PACKAGE_DIR},
+        package_data={"": DATA_FILES},
+        scripts=[STARTUP_SCRIPT],
+        cmdclass={"build_py": Cython},
     )
 
-    if 'build_ext' in ret.have_run.keys():
-        print 'Clean template files.'
-        ret.get_command_obj('build_py').cleanup_templatefiles()
+    if "build_ext" in ret.have_run.keys():
+        print("Clean template files.")
+        ret.get_command_obj("build_py").cleanup_templatefiles()
 
-    if 'bdist' in ret.commands:
-        BUILD_PATH = ret.get_command_obj('build_py').build_lib
+    if "bdist" in ret.commands:
+        BUILD_PATH = ret.get_command_obj("build_py").build_lib
         pyinstaller_run(BUILD_PATH)
